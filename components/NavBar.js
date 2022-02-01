5d49d6573887a05330d86fc7f95ab6fddc7c3417
최은정 <qhdrn67@naver.com>
2022-02-01 23:23:01 +0900

    Styling NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,6 +5,7 @@ export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
+      <img src='/vercel.svg' />
       <Link href='/'>
         <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
@@ -12,8 +13,27 @@ export default function NavBar() {
         <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
       <style jsx>{`
-        a {
-          text-decoration: none;
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
         .active {
           color: tomato;
